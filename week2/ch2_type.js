74599d7fa2f1a7829f013c9057ba73bc2372349c
William L Anderson <91501606+sstar691969@users.noreply.github.com>
2023-04-24 23:47:24 -0700

    1st commit
    chapter 2: week 2, assignment 1 and 2 completed.

---
+++ week2/ch2_type.js
@@ -0,0 +1,28 @@
+//let strToNr2 = "hello";
+//strToNr2 = Number(strToNr2);
+//console.log(strToNr2, typeof strToNr2);
+
+
+
+//let nr1 = 2;
+//let nr2 = "2";
+//console.log(nr1, typeof nr2);
+
+// Please use single lines to input example: console.log(str1, typeof str1);
+
+let str1 = 'Laurence';
+let str2 = "Svekis";
+let val1 = undefined;
+let val2 = null
+let myNum = 1000
+//example #2 and below: console.log("the value type of str1", typeof str2);
+//console.log("the value type of str2", typeof str2);
+//console.log("the value type of val1", typeof str2);
+//console.log(typeof myNum);
+
+console.log(str1, typeof str1);
+console.log(str2, typeof str2);
+//console.log(val1, typeof val1);
+//console.log(val2, typeof val2);
+//console.log(myNum, typeof str2);
+
