b1e8003360cb307632080c22893d17a76cbcc519
William L Anderson <91501606+sstar691969@users.noreply.github.com>
2023-04-28 10:39:57 -0700

    2nd commit
    final assignments 1 and 2 of ch3

---
+++ ch3_assign1.js
@@ -0,0 +1,37 @@
+const people = {friends:[ ]};
+    const person1 = {Firstname: "Rahel",
+                    Lastname: "Alemayehu",
+                    Id: 1
+
+
+
+
+
+
+};
+const person2 = {Firstname: "Johnny",
+                    Lastname: "Davis",
+                    Id: 2
+
+
+
+
+
+
+};
+   
+
+const person3 = {Firstname: "Pauline",
+                    Lastname: "Anderson",
+                    Id: 3
+
+
+
+
+
+
+};
+
+people.friends.push(person1, person2, person3);
+   
+console.log(people.friends);
