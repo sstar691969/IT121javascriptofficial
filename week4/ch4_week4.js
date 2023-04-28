93d62c98a6cf8b754a60f588aae2f42c2b78a70c
William L Anderson <91501606+sstar691969@users.noreply.github.com>
2023-04-28 10:56:42 -0700

    1st commit
    chapter 4 assignment final code

---
+++ week4/ch4_week4.js
@@ -0,0 +1,23 @@
+let rain = true; //can change to “false” it will print else statement
+if(rain) {  
+console.log("** Taking my umbrella when I need to go outside **");} 
+else {  
+console.log("** I can leave my umbrella at home **");
+} 
+
+
+
+//let hot = true 
+//if(hot) {  
+   // console.log("** Turn on a/c **");} 
+   // else {  
+   // console.log("** go jogging if not hot **");
+   // } 
+
+
+let hot = true 
+if(myVariable) {  
+        console.log("** Turn on a/c **");} 
+        if (!){  
+        console.log("** go jogging if not hot **");
+        } 
