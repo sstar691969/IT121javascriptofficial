57dd699dc120459eb1f892971fefabe7d1cc26f1
William L Anderson <91501606+sstar691969@users.noreply.github.com>
2023-05-13 00:40:47 -0700

    commit week5 assignment1 and 2
    final complete ..

---
+++ week5/ch5_assig_1final.js
@@ -0,0 +1,33 @@
+let array1 = [];
+
+
+
+results = 9
+
+ for(number1 = 0; number1 <= results;number1 ++)
+ 
+ {
+    let tempArray =[];
+
+
+   array1.push(tempArray)
+
+
+
+
+    for(number2 = 0; number2 <= results;number2 ++)
+    
+{ 
+
+    tempArray.push(number1 * number2); 
+        
+    console.log(tempArray,array1);
+
+
+    }
+    
+
+
+
+
+}   console.log(array1);
