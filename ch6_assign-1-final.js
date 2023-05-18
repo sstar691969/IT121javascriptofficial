aab1aee306d4b22a9569aa8137d76e4c19c7919e
William L Anderson <91501606+sstar691969@users.noreply.github.com>
2023-05-17 22:20:55 -0700

    commit final chp 6 assignment 1
    functions was remove from being within functions and the  instructions was created  functions for words.

---
+++ ch6_assign-1-final.js
@@ -0,0 +1,68 @@
+
+// i is equal to or greater than 10. when i is <= 10, it logs the current value of i to console.//
+function count(i) {
+    if (i >= 11) return;
+    console.log(i);
+    count(i + 1);  
+} 
+count(1);
+
+//below is a test for 2nd part or assignment//
+    
+//test below only//
+function four() {
+    console.log('four')
+   
+} 
+four();
+
+function one () {
+    console.log('one');
+      
+} 
+one();
+setTimeout(() => {
+    console.log('one');
+
+}, "3000");
+
+
+function three() {
+    console.log('three')
+   
+} 
+three();
+
+
+
+ 
+function two(){
+    console.log('two')
+    
+} 
+two(1);
+
+
+
+//function  one() {
+   // console.log('one')
+    
+//} 
+//one();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
